58b778c54882dfc5eef6e878cfefe26440c483ed
Louis Orleans <louis@orleans.io>
2023-08-25 12:35:17 -0700

    ♻️ use mapping

--- hosts/darwin/configuration.nix
+++ hosts/darwin/configuration.nix
@@ -162,35 +162,28 @@
       CustomUserPreferences = {
         NSGlobalDomain = {
           # NSUserDictionaryReplacementItems =
-          #   let
-          #     on = input:
+          #   map
+          #     (input:
           #       {
           #         on = 1;
           #         replace = builtins.elemAt input 0;
           #         "with" = builtins.elemAt input 1;
-          #       };
-          #   in
-          #   [
-          #     (on [ "fuck" "fuck" ])
-          #     (on [ "suck" "suck" ])
-          #     (on [ "fucking" "fucking" ])
-          #     # 😀
-          #     (on [ ":D" "\\Ud83d\\Ude00" ])
-          #     # 😛
-          #     (on [ ":p" "\\Ud83d\\Ude1b" ])
-          #     (on [ "idk" "IDK" ])
-          #     (on [ "*plusminus" "\\U00b1" ])
-          #     (on [ "gql" "GraphQL" ])
-          #     (on [ "omw" "on my way" ])
-          #     # ( ͡° ͜ʖ ͡°)
-          #     (on [ "*lenny" "( \\U0361\\U00b0 \\U035c\\U0296 \\U0361\\U00b0)" ])
-          #     # (╯°□°)╯︵ ┻━┻
-          #     (on [ "*tableflip" "(\\U256f\\U00b0\\U25a1\\U00b0)\\U256f\\Ufe35 \\U253b\\U2501\\U253b" ])
-          #     # ¯\_(ツ)_/¯
-          #     (on [ "*shruggie" "\\U00af\\\\_(\\U30c4)_/\\U00af" ])
-          #     # ( ಠ ಠ )
-          #     (on [ "*confused" "( \\U0ca0 \\U0ca0 )" ])
-          #   ];
+          #       })
+          #     [
+          #       [ "fuck" "fuck" ]
+          #       [ "suck" "suck" ]
+          #       [ "fucking" "fucking" ]
+          #       [ ":D" "\\Ud83d\\Ude00" ] # 😀
+          #       [ ":p" "\\Ud83d\\Ude1b" ] # 😛
+          #       [ "idk" "IDK" ]
+          #       [ "*plusminus" "\\U00b1" ]
+          #       [ "gql" "GraphQL" ]
+          #       [ "omw" "on my way" ]
+          #       [ "*lenny" "( \\U0361\\U00b0 \\U035c\\U0296 \\U0361\\U00b0)" ] # ( ͡° ͜ʖ ͡°)
+          #       [ "*tableflip" "(\\U256f\\U00b0\\U25a1\\U00b0)\\U256f\\Ufe35 \\U253b\\U2501\\U253b" ] # (╯°□°)╯︵ ┻━┻
+          #       [ "*shruggie" "\\U00af\\\\_(\\U30c4)_/\\U00af" ] # ¯\_(ツ)_/¯
+          #       [ "*confused" "( \\U0ca0 \\U0ca0 )" ] # ( ಠ ಠ )
+          #     ];
 
           # NSUserKeyEquivalents = {
           #   "Look Up in Dash" = "^h";
