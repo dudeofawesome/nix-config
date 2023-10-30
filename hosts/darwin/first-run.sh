02fbf4a75c55ddaf543da89124fda25015f7bc44
Louis Orleans <louis@orleans.io>
2023-10-30 13:47:34 -0700

    🐛 try to enable nix command

--- hosts/darwin/first-run.sh
+++ hosts/darwin/first-run.sh
@@ -50,7 +50,7 @@ function install_nix_darwin {
     # https://github.com/LnL7/nix-darwin#step-1-creating-flakenix
     $dryrun mkdir -p "$base_config"
     $dryrun pushd "$base_config"
-    $dryrun nix --experimental-features 'nix-command flakes' flake init -t nix-darwin
+    $dryrun nix --extra-experimental-features 'nix-command flakes' flake init -t nix-darwin
     $dryrun sed -i '' "s/simple/$(scutil --get LocalHostName)/" flake.nix
     if [[ "$(uname -p)" == "arm" ]]; then
       # switch to arm
