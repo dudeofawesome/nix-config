8788a0847433cbfc2229bd431510862cc39d9a93
Louis Orleans <louis@orleans.io>
2023-12-07 11:08:41 -0800

    ✨ add VM tools

--- modules/machine-classes/vm.nix
+++ modules/machine-classes/vm.nix
@@ -3,6 +3,10 @@
 {
   environment = {
     systemPackages = with pkgs; [
+      spice-vdagent
     ];
   };
+
+  services.qemuGuest.enable = true;
+  virtualisation.qemu.guestAgent.enable = true;
 }
