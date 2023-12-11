43a264d8e3c36249d62602ee80b65db9f3a218b3
Louis Orleans <louis@orleans.io>
2023-12-11 09:40:46 -0800

    💡 add comments about not changing `stateVersion`s

--- users/dudeofawesome/settings/default.nix
+++ users/dudeofawesome/settings/default.nix
@@ -8,7 +8,15 @@
   ];
 
   home = {
-    stateVersion = "23.05";
+    # It is occasionally necessary for Home Manager to change configuration
+    # defaults in a way that is incompatible with stateful data. This could, for
+    # example, include switching the default data format or location of a file.
+    #
+    # The state version indicates which default settings are in effect and will
+    # therefore help avoid breaking program configurations. Switching to a
+    # higher state version typically requires performing some manual steps,
+    # such as data conversion or moving files.
+    stateVersion = "23.05"; # Did you read the comment?
 
     keyboard = {
       layout = "us";
