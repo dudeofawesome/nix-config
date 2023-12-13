1bbc723af4a453d4253f3b8a5968b460de974d2e
Louis Orleans <louis@orleans.io>
2023-12-13 08:26:48 -0800

    🔧 update ZFS conf

--- modules/fs/zfs.nix
+++ modules/fs/zfs.nix
@@ -4,8 +4,12 @@
     zfs
   ];
 
-  # boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
-  # TODO: services.zfs.trim.enable = true;
+  # Enable auto-trim for SSDs.
+  services.zfs.trim.enable = true;
+  # Enable auto-scrub.
+  services.zfs.autoScrub.enable = true;
+
   # TODO: services.sanoid
 }
