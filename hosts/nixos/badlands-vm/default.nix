e1e07e886f5d19c4252a589b8e260c6513e97fa8
Louis Orleans <louis@orleans.io>
2023-12-13 19:50:00 -0800

    🐛 fix ZFS

--- hosts/nixos/badlands-vm/default.nix
+++ hosts/nixos/badlands-vm/default.nix
@@ -5,11 +5,13 @@
     ../../../modules/boot/systemd-boot.nix
     ../../../modules/machine-classes/base.nix
     ../../../modules/machine-classes/local-vm.nix
+    ../../../modules/fs/zfs.nix
     ../../../users/dudeofawesome/os/linux.nix
   ];
 
   networking = {
     hostName = "badlands-vm";
+    hostId = "503a29b9"; # head -c 8 /etc/machine-id
     firewall.enable = false;
   };
 
--- modules/fs/zfs.nix
+++ modules/fs/zfs.nix
@@ -1,10 +1,27 @@
 { config, pkgs, ... }:
+let
+  # Linux6.2 and later add DRM to exported symbols, which are required on aarch64
+  removeLinuxDRM =
+    pkgs.stdenv.targetPlatform.isAarch64
+    && ((builtins.compareVersions pkgs.linux.version "6.2") != -1);
+in
 {
   environment.systemPackages = with pkgs; [
     zfs
   ];
 
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot = {
+    zfs = {
+      removeLinuxDRM = removeLinuxDRM;
+      allowHibernation = false;
+    };
+
+    kernelPackages = (pkgs.zfs.override {
+      removeLinuxDRM = removeLinuxDRM;
+    }).latestCompatibleLinuxPackages;
+
+    supportedFilesystems = [ "zfs" ];
+  };
 
   # Enable auto-trim for SSDs.
   services.zfs.trim.enable = true;
