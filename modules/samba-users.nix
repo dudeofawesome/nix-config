6b78c6f732cd1c10b511c47dd45f759f638464ba
Louis Orleans <louis@orleans.io>
2023-12-24 13:22:16 -0800

    ✨ add time machine & smb users modules

---
+++ modules/samba-users.nix
@@ -0,0 +1,81 @@
+{ lib, config, pkgs, ... }:
+with lib; let
+  cfg = config.services.samba.users;
+
+  userOpts = { name, config, ... }: {
+    options = {
+      name = mkOption {
+        type = types.passwdEntry types.str;
+        apply = x: assert (builtins.stringLength x < 32 || abort "Username '${x}' is longer than 31 characters which is not allowed!"); x;
+        description = lib.mdDoc ''
+          The name of the user account. If undefined, the name of the
+          attribute set will be used.
+        '';
+      };
+
+      plaintextPasswordFile = mkOption {
+        type = with types; nullOr str;
+        default = cfg.users.${name}.passwordFile;
+        defaultText = literalExpression "null";
+        description = lib.mdDoc ''
+          The full path to a file that contains the plaintext of the user's
+          password. The password file is read on each system activation. The
+          file should contain exactly one line, which should be the password in
+          plaintext that is suitable for the `smbpasswd` command.
+          ${passwordDescription}
+        '';
+      };
+    };
+
+    config = {
+      name = mkDefault name;
+    };
+  };
+in
+{
+  options = {
+    services.samba.users = {
+      enable = mkEnableOption "Declarative Samba user management";
+
+      users = mkOption {
+        default = { };
+        type = with types; attrsOf (submodule userOpts);
+        example = {
+          alice = {
+            plaintextPasswordFile = "/secrets/alice";
+          };
+        };
+        description = lib.mdDoc ''
+          Additional user accounts to be created automatically by the system.
+          This can also be used to set options for root.
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      # Add the samba CLI to ensure access to `smbpasswd`.
+      samba
+    ];
+
+    # create smb users
+    system.activationScripts.time-machine_set-passwords =
+      concatMapStringsSep "; "
+        (key:
+          let val = cfg.users.${key}; in
+          ''
+            { cat '${val.plaintextPasswordFile}'; echo ""; cat '${val.plaintextPasswordFile}'; echo ""; } \
+              | "${pkgs.samba}/bin/smbpasswd" -s -a '${val.name}'
+          '')
+        (builtins.attrNames cfg.users);
+
+    # users.users = builtins.mapAttrs
+    #   (key: val: if config.users.users.${val.name} then { } else {
+    #     name = val.name;
+    #     isSystemUser = true;
+    #     group = cfg.systemUser;
+    #   })
+    #   cfg.users;
+  };
+}
---
+++ modules/time-machine-server.nix
@@ -0,0 +1,76 @@
+{ lib, config, pkgs, ... }:
+with lib; let
+  cfg = config.services.samba.time-machine;
+in
+{
+  options = {
+    services.samba.time-machine = {
+      enable = mkEnableOption "Apple Time Machine backup server";
+
+      baseDir = mkOption {
+        type = types.str;
+        example = "/mnt/time-machine";
+        description = ''
+          The directory under which user's individual shares should be created.
+        '';
+      };
+
+      group = mkOption {
+        type = types.str;
+        default = "time-machine";
+        description = ''
+          The username that should own the files on disk.
+        '';
+      };
+
+      users = mkOption {
+        default = [ ];
+        type = types.listOf types.str;
+        example = [ "dudeofawesome" ];
+        description = lib.mdDoc ''
+          Usernames that should be allowed to back up to the Time Machine share.
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      # Add the samba CLI utils to enable debugging.
+      samba
+    ];
+
+    services = {
+      samba = {
+        enable = true;
+        openFirewall = true;
+        shares = {
+          "Time Machine" = {
+            path = cfg.baseDir;
+            comment = "Remote Time Machine target";
+            "valid users" = concatStringsSep "," cfg.users;
+            public = "no";
+            writeable = "yes";
+            "fruit:aapl" = "yes";
+            "fruit:time machine" = "yes";
+            "vfs objects" = "catia fruit streams_xattr";
+          };
+        };
+      };
+
+      avahi.enable = true;
+    };
+
+    # create service group
+    users.groups.${cfg.group} = {
+      members = cfg.users;
+    };
+
+    # create directory
+    system.activationScripts.time-machine_mkdir = ''
+      mkdir -p "${cfg.baseDir}"
+      chown root:${cfg.group} "${cfg.baseDir}"
+      chmod g+rw "${cfg.baseDir}"
+    '';
+  };
+}
