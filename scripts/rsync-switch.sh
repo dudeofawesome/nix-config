eb802fbd90f3c0bb697a9d887f93faae40e5ab24
Louis Orleans <louis@orleans.io>
2023-12-25 22:05:24 -0800

    🔨 add command option

--- scripts/rsync-switch.sh
+++ scripts/rsync-switch.sh
@@ -2,8 +2,9 @@
 set -e
 
 hostname="$1"
-path="${2:-"~/git/dudeofawesome/nix-config"}"
+command="${2:-"switch"}"
+path="${3:-"~/git/dudeofawesome/nix-config"}"
 
 ssh "$hostname" -t -C "mkdir -p $path" > /dev/null
 rsync --delete --archive ./ "$hostname":"$path"
-ssh "$hostname" -t -C 'cd '"$path"' && sudo nixos-rebuild switch --flake .#'"$hostname"' --show-trace'
+ssh "$hostname" -t -C 'cd '"$path"' && sudo nixos-rebuild '"$command"' --flake .#'"$hostname"' --show-trace'
