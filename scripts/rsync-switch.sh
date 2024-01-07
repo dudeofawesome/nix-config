679537437dc714e801d93c935a1c8a71bdba3908
Louis Orleans <louis@orleans.io>
2024-01-07 15:19:11 -0800

    ✨ support pushing to host by IP

--- scripts/rsync-switch.sh
+++ scripts/rsync-switch.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-hostname="$1"
+hostname="$(echo "$1" | cut -d '=' -f1)"
+ssh_conn="$(echo "$1" | cut -d '=' -s -f2)"
+ssh_conn="${ssh_conn:-"$hostname"}"
 command="${2:-"switch"}"
 path="${3:-"~/git/dudeofawesome/nix-config"}"
 
-ssh "$hostname" -t -C "mkdir -p $path" > /dev/null
-rsync --delete --archive ./ "$hostname":"$path"
-ssh "$hostname" -t -C 'cd '"$path"' && sudo nixos-rebuild '"$command"' --flake .#'"$hostname"' --show-trace'
+ssh "$ssh_conn" -t -C "mkdir -p $path" > /dev/null
+rsync --delete --archive ./ "$ssh_conn":"$path"
+ssh "$ssh_conn" -t -C 'cd '"$path"' && sudo nixos-rebuild '"$command"' --flake .#'"$hostname"' --show-trace'
