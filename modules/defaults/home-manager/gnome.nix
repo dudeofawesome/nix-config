d2e385b941fe07dda762e94d31e6d7a649efa62c
Louis Orleans <louis@orleans.io>
2024-01-10 00:18:22 -0800

    🐛 only gnome if gnome

--- modules/defaults/home-manager/gnome.nix
+++ modules/defaults/home-manager/gnome.nix
@@ -1,16 +1,18 @@
 { pkgs, lib, osConfig, ... }: {
-  dconf.settings = lib.mkIf pkgs.stdenv.targetPlatform.isLinux {
-    "org/gnome/shell" = {
-      favorite-apps = [
-        "org.gnome.Nautilus.desktop"
-        "firefox.desktop"
-        "org.gnome.Console.desktop"
-        "steam.desktop"
-        "org.gnome.Settings.desktop"
-      ];
-    };
-    "org/gnome/desktop/interface" = {
-      monospace-font-name = (builtins.elemAt osConfig.fonts.packages 0).name;
+  config = lib.mkIf osConfig.services.xserver.desktopManager.gnome.enable {
+    dconf.settings = {
+      "org/gnome/shell" = {
+        favorite-apps = [
+          "org.gnome.Nautilus.desktop"
+          "firefox.desktop"
+          "org.gnome.Console.desktop"
+          "steam.desktop"
+          "org.gnome.Settings.desktop"
+        ];
+      };
+      "org/gnome/desktop/interface" = {
+        monospace-font-name = (builtins.elemAt osConfig.fonts.packages 0).name;
+      };
     };
   };
 }
