f4d01290172859e959cf4b3dbcdc4580dfb80648
Louis Orleans <louis@orleans.io>
2024-01-13 00:49:29 -0800

    ✨ add bcachefs module

---
+++ modules/defaults/fs/bcachefs.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, config }:
+let
+  # TODO: figure out a way to detect if a linuxPackage is LTS
+  prefer_lts = false;
+  lts_has_bcachefs = (builtins.compareVersions pkgs.linuxPackages.kernel.version "6.7") >= 0;
+  kernelPackage =
+    if (lts_has_bcachefs && prefer_lts) then pkgs.linuxPackages
+    else pkgs.linuxPackages_latest;
+in
+{
+  environment.systemPackages = with pkgs; [
+    bcachefs-tools
+  ];
+
+  boot = {
+    kernelPackages = lib.mkDefault kernelPackage;
+    supportedFilesystems = [ "bcachefs" ];
+  };
+}
