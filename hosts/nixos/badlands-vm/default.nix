d7415981c9950652cdaf01759bcd2005ad7f0667
Louis Orleans <louis@orleans.io>
2024-01-17 16:59:08 -0800

    🐛 skip zfs & bcachefs for now

--- hosts/nixos/badlands-vm/default.nix
+++ hosts/nixos/badlands-vm/default.nix
@@ -1,9 +1,4 @@
 { config, pkgs, ... }: {
-  imports = [
-    ../../../modules/defaults/fs/zfs.nix
-    ../../../modules/defaults/fs/bcachefs.nix
-  ];
-
   networking = {
     hostId = "503a29b9"; # head -c 8 /etc/machine-id
     firewall.enable = false;
