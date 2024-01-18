80765fd7fb1f7f4d661780a1ca7c77bb160bff15
Louis Orleans <louis@orleans.io>
2024-01-17 16:39:29 -0800

    🔨 add nixos-anywhere + sops script

---
+++ scripts/nixos-anywhere.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+flake="$1"
+ssh_conn="$2"
+
+# TODO: get the path from the config
+fde_password_file="/run/secrets/hosts/nixos/$flake/fde_password"
+fde_password_dir="$(dirname "$fde_password_file")"
+
+sudo mkdir -p "$fde_password_dir"
+sudo chown "$USER" "$fde_password_dir"
+sops \
+  --decrypt \
+  --extract '["hosts"]["nixos"]["'"$flake"'"]["fde_password"]' \
+  --output "$fde_password_file" \
+  "hosts/nixos/$flake/secrets.yaml"
+
+nix run github:nix-community/nixos-anywhere -- \
+  --flake .#"$flake" \
+  --disk-encryption-keys \
+    `# remote path` \
+    "/tmp/fde_password" \
+    `# local path` \
+    "$fde_password_file" \
+  --debug \
+  "$ssh_conn"
