c1c7f37d7cf4c76c4a49d9625101977656c81ef1
Louis Orleans <louis@orleans.io>
2024-10-02 23:08:45 -0700

    🔧 set darwinConfig path

--- modules/defaults/base/darwin.nix
+++ modules/defaults/base/darwin.nix
@@ -3,6 +3,10 @@
     ../../configurable/hosts.darwin.nix
   ];
 
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
   fonts = {
     packages = with pkgs; [
       (nerdfonts.override {
