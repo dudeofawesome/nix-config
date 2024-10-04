c13724b9fff043dda7333423ae177894ee84fc26
Louis Orleans <louis@orleans.io>
2024-10-03 23:58:40 -0700

    🎨

--- modules/defaults/home-manager/fork.nix
+++ modules/defaults/home-manager/fork.nix
@@ -1,16 +1,14 @@
 { pkgs, lib, config, ... }:
 {
   targets.darwin = lib.mkIf pkgs.stdenv.targetPlatform.isDarwin {
-    defaults = {
-      "com.DanPristupov.Fork" = {
-        pullSheetRebase = true;
-        pullSheetStashAndReapply = true;
-        rebaseSheetAutostash = true;
-        fetchAllTags = true;
-        fetchSheetFetchAllRemotes = false;
-        deleteStashAfterApply = true;
-        defaultSourceFolder = "/Users/${config.home.username}/git";
-      };
+    defaults."com.DanPristupov.Fork" = {
+      pullSheetRebase = true;
+      pullSheetStashAndReapply = true;
+      rebaseSheetAutostash = true;
+      fetchAllTags = true;
+      fetchSheetFetchAllRemotes = false;
+      deleteStashAfterApply = true;
+      defaultSourceFolder = "/Users/${config.home.username}/git";
     };
   };
 }
