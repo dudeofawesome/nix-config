e13e1a660c5a46204e29758959f0f8d4761b1281
Louis Orleans <louis@orleans.io>
2024-10-03 23:58:01 -0700

    ♻️ use targets.defaults

--- modules/defaults/home-manager/hammerspoon.nix
+++ modules/defaults/home-manager/hammerspoon.nix
@@ -1,13 +1,15 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
+  targets.darwin = lib.mkIf pkgs.stdenv.targetPlatform.isDarwin {
+    defaults."org.hammerspoon.Hammerspoon" = {
+      MJConfigFile = "/Users/${config.home.username}/.config/hammerspoon/init.lua";
+    };
+  };
+
   home = lib.mkIf pkgs.stdenv.targetPlatform.isDarwin {
     file.hammerspoon = {
       target = ".config/hammerspoon/";
       source = "${pkgs.dotfiles.dudeofawesome}/settings/hammerspoon/";
     };
-
-    activation.hammerspoon = ''
-      PATH="/usr/bin:$PATH" $DRY_RUN_CMD defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
-    '';
   };
 }
--- modules/defaults/home-manager/middleclick.nix
+++ modules/defaults/home-manager/middleclick.nix
@@ -1,9 +1,15 @@
 { pkgs, lib, ... }:
 {
+  targets.darwin = lib.mkIf pkgs.stdenv.targetPlatform.isDarwin {
+    defaults."com.rouge41.middleClick" = {
+      needClick = false;
+      fingers = 4;
+    };
+  };
+
   home = lib.mkIf pkgs.stdenv.targetPlatform.isDarwin {
     activation.middleclick = ''
       PATH="/usr/bin:$PATH" $DRY_RUN_CMD osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/MiddleClick.app", hidden:true}'
-      PATH="/usr/bin:$PATH" $DRY_RUN_CMD defaults write com.rouge41.middleClick fingers 4
     '';
   };
 }
