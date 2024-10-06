f284f029dfe6f83d4ef9b477c802ee17596f928e
Louis Orleans <louis@orleans.io>
2024-10-06 10:55:15 -0700

    ✨ add more fork settings

--- modules/defaults/home-manager/fork.nix
+++ modules/defaults/home-manager/fork.nix
@@ -3,13 +3,23 @@
   config = lib.mkIf pkgs.stdenv.targetPlatform.isDarwin {
     targets.darwin = {
       defaults."com.DanPristupov.Fork" = {
+        defaultSourceFolder = "/Users/${config.home.username}/git";
+        NSNavLastRootDirectory = "~/git";
+
+        # rebase + stash & apply
         pullSheetRebase = true;
         pullSheetStashAndReapply = true;
         rebaseSheetAutostash = true;
+        checkoutBranchSheetStashAndReapply = true;
+        deleteStashAfterApply = false;
+        trackBranchSheetStashAndReapply = true;
+
         fetchAllTags = true;
         fetchSheetFetchAllRemotes = false;
-        deleteStashAfterApply = true;
-        defaultSourceFolder = "/Users/${config.home.username}/git";
+        createBranchSheetCheckout = true;
+
+        diffIgnoreWhitespaces = true;
+        diffShowHiddenSymbols = true;
       };
     };
   };
