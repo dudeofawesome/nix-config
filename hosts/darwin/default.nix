b38d752aa744740a4ea8a5e2180d79c04a9739ae
Louis Orleans <louis@orleans.io>
2024-11-12 13:25:10 -0800

    ♻️ DRY hosts

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -3,46 +3,40 @@
 , usersModule
 , packageOverlays
 , ...
-}: {
-  crater-lake = inputs.darwin.lib.darwinSystem (import ../system.nix {
+}:
+let
+  base = {
     inherit
       inputs
       lib
       packageOverlays
       ;
+
+    os = "darwin";
+  };
+in
+{
+  crater-lake = inputs.darwin.lib.darwinSystem (import ../system.nix ({
     hostname = "crater-lake";
     arch = "aarch64";
-    os = "darwin";
     owner = "dudeofawesome";
     machine-class = "pc";
     users = usersModule.filterMap [ "dudeofawesome" ] usersModule.users;
-  });
+  } // base));
 
-  badlands = inputs.darwin.lib.darwinSystem (import ../system.nix {
-    inherit
-      inputs
-      lib
-      packageOverlays
-      ;
+  badlands = inputs.darwin.lib.darwinSystem (import ../system.nix ({
     hostname = "badlands";
     arch = "aarch64";
-    os = "darwin";
     owner = "dudeofawesome";
     machine-class = "pc";
     users = usersModule.filterMap { dudeofawesome = "lorleans"; } usersModule.users;
-  });
+  } // base));
 
-  joshs-paciolan-laptop = inputs.darwin.lib.darwinSystem (import ../system.nix {
-    inherit
-      inputs
-      lib
-      packageOverlays
-      ;
+  joshs-paciolan-laptop = inputs.darwin.lib.darwinSystem (import ../system.nix ({
     hostname = "joshs-paciolan-laptop";
     arch = "aarch64";
-    os = "darwin";
     owner = "josh";
     machine-class = "pc";
     users = usersModule.filterMap { josh = "joshuagibbs"; } usersModule.users;
-  });
+  } // base));
 }
--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -3,88 +3,64 @@
 , usersModule
 , packageOverlays
 , ...
-}: {
-  kings-canyon = lib.nixosSystem (import ../system.nix {
+}:
+let
+  base = {
     inherit
       inputs
       lib
       packageOverlays
       ;
+
+    os = "linux";
+  };
+in
+{
+  kings-canyon = lib.nixosSystem (import ../system.nix ({
     hostname = "kings-canyon";
     arch = "x86_64";
-    os = "linux";
     owner = "dudeofawesome";
     machine-class = "server";
     users = usersModule.filterMap [ "dudeofawesome" ] usersModule.users;
-  });
+  } // base));
 
-  badlands-vm = lib.nixosSystem (import ../system.nix {
-    inherit
-      inputs
-      lib
-      packageOverlays
-      ;
+  badlands-vm = lib.nixosSystem (import ../system.nix ({
     hostname = "badlands-vm";
     arch = "aarch64";
-    os = "linux";
     owner = "dudeofawesome";
     machine-class = "local-vm";
     users = usersModule.filterMap [ "dudeofawesome" ] usersModule.users;
-  });
+  } // base));
 
-  monongahela = lib.nixosSystem (import ../system.nix {
-    inherit
-      inputs
-      lib
-      packageOverlays
-      ;
+  monongahela = lib.nixosSystem (import ../system.nix ({
     hostname = "monongahela";
     arch = "x86_64";
-    os = "linux";
     owner = "dudeofawesome";
     machine-class = "server";
     users = usersModule.filterMap [ "dudeofawesome" ] usersModule.users;
-  });
+  } // base));
 
-  haleakala = lib.nixosSystem (import ../system.nix {
-    inherit
-      inputs
-      lib
-      packageOverlays
-      ;
+  haleakala = lib.nixosSystem (import ../system.nix ({
     hostname = "haleakala";
     arch = "x86_64";
-    os = "linux";
     owner = "dudeofawesome";
     machine-class = "pc";
     users = usersModule.filterMap [ "dudeofawesome" ] usersModule.users;
-  });
+  } // base));
 
-  haleakala-sim = lib.nixosSystem (import ../system.nix {
-    inherit
-      inputs
-      lib
-      packageOverlays
-      ;
+  haleakala-sim = lib.nixosSystem (import ../system.nix ({
     hostname = "haleakala-sim";
     arch = "aarch64";
-    os = "linux";
     owner = "dudeofawesome";
     machine-class = "pc";
     users = usersModule.filterMap [ "dudeofawesome" ] usersModule.users;
-  });
+  } // base));
 
-  soto-server = lib.nixosSystem (import ../system.nix {
-    inherit
-      inputs
-      lib
-      packageOverlays
-      ;
+  soto-server = lib.nixosSystem (import ../system.nix ({
     hostname = "soto-server";
     arch = "x86_64";
-    os = "linux";
     owner = "josh";
     machine-class = "server";
     users = usersModule.filterMap [ "josh" ] usersModule.users;
-  });
+  } // base));
 }
