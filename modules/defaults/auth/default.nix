9324726a964d910ec11dcb72c1e9e49c6674797f
Louis Orleans <louis@orleans.io>
2025-01-01 23:52:27 -0800

    ✨ add default os option

--- modules/defaults/auth/default.nix
+++ modules/defaults/auth/default.nix
@@ -10,6 +10,11 @@
   ];
 
   config = lib.concatMapAttrs (
-    key: val: if (val.config ? "${os}") then val.config.${os} else { }
+    key: val:
+    (
+      { }
+      // (if (val.config ? "default") then val.config.default else { })
+      // (if (val.config ? "${os}") then val.config.${os} else { })
+    )
   ) users;
 }
