9ed25902756323287c9e0ecf76bd50973b4fdab5
Louis Orleans <louis@orleans.io>
2025-02-06 16:23:42 -0800

    🐛 verify that user has os-level config

--- modules/defaults/auth/default.nix
+++ modules/defaults/auth/default.nix
@@ -12,10 +12,13 @@
 
   config = lib.concatMapAttrs (
     key: val:
-    (
-      { }
-      // (if (val.os ? "default") then (val.os.default { inherit config lib; }) else { })
-      // (if (val.os ? "${os}") then (val.os.${os} { inherit config lib; }) else { })
-    )
+    if (val ? "os") then
+      (
+        { }
+        // (if (val.os ? "default") then (val.os.default { inherit config lib; }) else { })
+        // (if (val.os ? "${os}") then (val.os.${os} { inherit config lib; }) else { })
+      )
+    else
+      ({ })
   ) users;
 }
