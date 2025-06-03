e7e147aaad15c7a4d90b742fa6d9b743ce08d088
Louis Orleans <louis@orleans.io>
2025-06-03 02:24:38 -0700

    🔨 add script to generate dependency graph

---
+++ scripts/mk-graph.fish
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S nix
+#! nix shell nixpkgs#fish nixpkgs#graphviz nixpkgs#gnused --command fish
+
+set output_svg ./generation.svg
+
+set generation (readlink /nix/var/nix/profiles/system/)
+
+set dot_file (
+  nix-store --query --graph "$generation" \
+  | sed -E \
+    -e '1a\
+  rankdir=BT;\
+  graph [ratio=0.9];\
+  bgcolor="#181818";\
+  node [\
+    fontcolor = "#e6e6e6",\
+    style = filled,\
+    color = "#e6e6e6",\
+    fillcolor = "#333333"\
+  ]\
+  edge [\
+    color = "#e6e6e6",\
+    fontcolor = "#e6e6e6"\
+    penwidth = 2.0\
+  ]' \
+    -e 's/fillcolor = "#ff0000"//g' \
+)
+
+echo "$dot_file" | dot -Tsvg >"$output_svg"
+echo "$dot_file" | dot -Tpng -Gdpi=10 >"generation.png"
