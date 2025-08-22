607a7053387077982f8068b7836b5e64031e4846
Louis Orleans <louis@orleans.io>
2025-08-22 13:52:58 -0700

    🐛 set docker client secret store

--- modules/configurable/home-manager/docker-client.nix
+++ modules/configurable/home-manager/docker-client.nix
@@ -6,6 +6,7 @@
 }:
 
 let
+  inherit (pkgs.stdenv.targetPlatform) isDarwin isLinux;
   cfg = config.programs.docker-client;
 in
 {
@@ -16,10 +17,43 @@ in
       package = lib.mkPackageOption pkgs "Docker Client" {
         default = [ "docker-client" ];
       };
+
+      dockerDir = lib.mkOption {
+        type = lib.types.str;
+        default = ".docker";
+      };
+
+      settings = lib.mkOption {
+        description = ''Settings for Docker Desktop's settings.json'';
+        type = lib.types.attrs;
+        default = {
+          credsStore =
+            if isLinux then
+              "pass"
+            else if isDarwin then
+              "osxkeychain"
+            else
+              abort;
+        };
+        example = {
+          credsStore = "pass";
+        };
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ cfg.package ];
+    home = {
+      packages = [
+        cfg.package
+        pkgs.docker-credential-helpers
+      ];
+
+      file-json.docker-client-settings = lib.mkIf (cfg.settings != { }) {
+        inherit (cfg) enable;
+        target = "${cfg.dockerDir}/config.json";
+        extraConfig = cfg.settings;
+      };
+    };
   };
 }
