1ed85f9fe3d5ddbb015ad416bc9003632f9225a2
Louis Orleans <louis@orleans.io>
2026-05-05 17:57:37 -0700

    ✨ create custom updater for claude-desktop

--- packages/claude-desktop/package.nix
+++ packages/claude-desktop/package.nix
@@ -1,8 +1,14 @@
 {
   lib,
   stdenv,
+  cacert,
+  curl,
   fetchurl,
+  gnused,
+  jq,
+  openssl,
   unzip,
+  writeShellApplication,
   ...
 }:
 
@@ -28,6 +34,38 @@ stdenv.mkDerivation (finalAttrs: {
 
     runHook postInstall
   '';
+
+  passthru.updateScript = lib.getExe (writeShellApplication {
+    name = "claude-desktop-update-script";
+    runtimeInputs = [
+      cacert
+      curl
+      gnused
+      jq
+      openssl
+    ];
+    text = ''
+      release_feed="https://downloads.claude.ai/releases/darwin/universal/RELEASES.json"
+
+      release=$(curl --fail --location --silent "$release_feed")
+
+      version=$(jq --raw-output '.currentRelease' <<< "$release")
+      url=$(jq --raw-output --arg version "$version" '.releases[] | select(.version == $version) | .updateTo.url' <<< "$release")
+      filename=''${url##*/}
+      replacement_url="https://downloads.claude.ai/releases/darwin/universal/\''${finalAttrs.version}/$filename"
+      hash="sha256-$(
+        curl --fail --location --silent "$url" \
+          | openssl dgst -sha256 -binary \
+          | openssl base64
+      )"
+
+      sed -i -E \
+        -e 's@(version = )"[0-9]+\.[0-9]+\.[0-9]+";@\1"'"$version"'";@' \
+        -e 's@(url = )"https://downloads\.claude\.ai/releases/darwin/universal/[^"]+";@\1"'"$replacement_url"'";@' \
+        -e 's@(hash = )("sha256-[A-Za-z0-9+/]+="|null);@\1"'"$hash"'";@' \
+        ./packages/claude-desktop/package.nix
+    '';
+  });
 
   meta = {
     description = "Anthropic Claude desktop app for macOS";
