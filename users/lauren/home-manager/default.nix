d10b07a8a91e53aed51ea4e888c4d8b063a2911e
Louis Orleans <louis@orleans.io>
2026-05-05 23:34:34 -0700

    🔧 add dev tools

--- users/lauren/home-manager/default.nix
+++ users/lauren/home-manager/default.nix
@@ -1,8 +1,13 @@
-{ lib, ... }:
+{ lib, pkgs-unstable, ... }:
 {
   imports = [
     ../../../modules/defaults/home-manager
+    ./vscode
   ];
 
   home.stateVersion = "23.05";
+
+  home.packages = with pkgs-unstable; [
+    devenv
+  ];
 }
