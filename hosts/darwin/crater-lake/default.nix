32f299a9fa6ab17bdc0a1ad87f1b2eff5236b7a6
Louis Orleans <louis@orleans.io>
2026-08-05 12:00:09 -0700

    🔧 disable TM auto-mount

--- hosts/darwin/crater-lake/default.nix
+++ hosts/darwin/crater-lake/default.nix
@@ -20,4 +20,20 @@
       devices = [ { device = "/dev/disk0"; } ];
     };
   };
+
+  environment.etc."fstab" = {
+    text = ''
+      # /etc/fstab: DO NOT EDIT -- this file has been generated automatically by nix-darwin.
+
+      UUID=fbfb8f37-d08d-4db9-b0d0-4b3456a7675c /nix apfs rw,noatime,noauto,nobrowse,nosuid,owners # Added by the Determinate Nix Installer
+
+      # disable auto-mounting Time Machine volumes for other systems
+      UUID=3594CA5E-6412-4E77-8BAA-C7FF37E9752B none apfs rw,noauto
+      UUID=F462EC05-1C97-408B-BC70-07C777A607AB none apfs rw,noauto
+    '';
+
+    knownSha256Hashes = [
+      "bb6777d92974cbef6a288879e27cd9306887a4ddbd5f25dea097cc0a063103e1"
+    ];
+  };
 }
