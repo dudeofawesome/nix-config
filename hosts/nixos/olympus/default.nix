545a6fd5e4832f956d2036ad50794eda8c8b2c8b
Louis Orleans <louis@orleans.io>
2026-08-05 02:14:58 -0700

    ✨💄 add plymouth theme

--- hosts/nixos/olympus/default.nix
+++ hosts/nixos/olympus/default.nix
@@ -150,6 +150,34 @@
         devices.${config.fileSystems."/".device}.secretFile = ./clevis.jwe;
       };
     };
+
+    plymouth = {
+      enable = true;
+
+      theme = "nixos-bgrt";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        # (adi1090x-plymouth-themes.override {
+        #   selected_themes = [ "rings" ];
+        # })
+        nixos-bgrt-plymouth
+      ];
+
+      # HiDPI
+      # extraConfig = ''
+      #   [Daemon]
+      #   DeviceScale=1
+      # '';
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
+    ];
   };
 
   environment.systemPackages = with pkgs; [
