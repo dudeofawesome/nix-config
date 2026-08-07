ae2f011894f941481d8ba26fe120f0d0ebcd334e
Louis Orleans <louis@orleans.io>
2026-08-07 01:58:19 -0700

    ✨ require IPs

--- modules/defaults/tang.nix
+++ modules/defaults/tang.nix
@@ -1,33 +1,34 @@
 { lib, config, ... }: {
-  # assertions = [
-  #   {
-  #     assertion = config.services.tang.ipAddressAllow != [ ];
-  #     message = "`services.tang.ipAddressAllow` must be set!";
-  #   }
-  # ];
-
   config = {
+    assertions = [
+      {
+        assertion = config.services.tang.ipAddressAllow != [ ];
+        message = "`services.tang.ipAddressAllow` must be set!";
+      }
+    ];
+
     services.tang = {
       enable = true;
-      # ipAddressAllow = [ "10.0.0.0/24" ];
     };
 
     # TODO: assign ports based on interface
-    networking.firewall.allowedTCPPorts = lib.pipe config.services.tang.listenStream [
-      (map (
-        stream:
-        if (lib.isInt stream) then
-          stream
-        else if true then
-          lib.pipe stream [
-            (builtins.split "\:(\d+$)")
-            (split: builtins.elemAt split 0)
-          ]
-        else
-          abort "Unsupported tang listenStream: ${stream}"
-      ))
+    networking.firewall.allowedTCPPorts = lib.mkIf (config.services.tang.enable) (
+      lib.pipe config.services.tang.listenStream [
+        (map (
+          stream:
+          if (lib.isInt stream) then
+            stream
+          else if true then
+            lib.pipe stream [
+              (builtins.split "\:(\d+$)")
+              (split: builtins.elemAt split 0)
+            ]
+          else
+            abort "Unsupported tang listenStream: ${stream}"
+        ))
 
-      (map lib.toInt)
-    ];
+        (map lib.toInt)
+      ]
+    );
   };
 }
