05bd9e1b4fe45c3d56ef087f5e1933a64462a6fb
Louis Orleans <louis@orleans.io>
2026-08-06 19:29:22 -0700

    🐛 fix shallow merge leading to unapplied settings

--- modules/defaults/auth/default.nix
+++ modules/defaults/auth/default.nix
@@ -10,15 +10,16 @@
     ./default.${os}.nix
   ];
 
-  config = lib.concatMapAttrs (
-    key: val:
-    if (val ? "os") then
-      (
+  config = lib.mkMerge (
+    lib.mapAttrsToList (
+      _key: val:
+      if val ? "os" then
+        lib.mkMerge [
+          (lib.optionalAttrs (val.os ? "default") (val.os.default { inherit config lib; }))
+          (lib.optionalAttrs (val.os ? "${os}") (val.os.${os} { inherit config lib; }))
+        ]
+      else
         { }
-        // (if (val.os ? "default") then (val.os.default { inherit config lib; }) else { })
-        // (if (val.os ? "${os}") then (val.os.${os} { inherit config lib; }) else { })
-      )
-    else
-      ({ })
-  ) users;
+    ) users
+  );
 }
