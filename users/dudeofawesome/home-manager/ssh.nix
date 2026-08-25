4cbea864baa8ef518ea572124e0096794e6f2d4d
Louis Orleans <louis@orleans.io>
2026-08-24 17:58:50 -0700

    ✨ add wake-on-lan for SSH

--- users/dudeofawesome/home-manager/ssh.nix
+++ users/dudeofawesome/home-manager/ssh.nix
@@ -2,6 +2,41 @@
   programs.ssh.settings =
     let
       hostUnreachable = (host: ''host ${host} !exec "ping -c1 -q -t1 '%h' 2> /dev/null"'');
+
+      wake = pkgs.writeShellScript "ssh-wake-on-lan.sh" ''
+        # @param host[:port]
+        # @param MAC address[es]
+
+        set -u
+
+        host_and_port="$1"
+        shift
+
+        if [[ "$host_and_port" == *:* ]]; then
+          host="''${host_and_port%:*}"
+          port="''${host_and_port##*:}"
+        else
+          host="$host_and_port"
+          port=22
+        fi
+
+        function is_reachable() {
+          ${lib.getExe pkgs.netcat} -z -w 1 "$host" "$port" \
+            > /dev/null 2>&1
+        }
+
+        if ! is_reachable; then
+          ${lib.getExe pkgs.wakeonlan} "$@"
+
+          attempts=0
+          until is_reachable || [[ "$attempts" -ge 60 ]]; do
+            sleep 1
+            attempts=$((attempts + 1))
+          done
+        fi
+
+        exec ${lib.getExe pkgs.netcat} "$host" "$port"
+      '';
     in
     {
       "*".User = "dudeofawesome";
@@ -18,9 +53,7 @@
       };
 
       "olympus" = {
-        ProxyCommand = ''
-          ${wake} 'olympus' 'c8:7f:54:6a:3f:56' 'c8:7f:54:6a:45:b7'
-        '';
+        ProxyCommand = "${wake} 'olympus' 'c8:7f:54:6a:3f:56' 'c8:7f:54:6a:45:b7'";
       };
 
       "home.powell.place".User = "louis";
