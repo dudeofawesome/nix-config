a8f29ab03d268a00eb7a17dd94765c58ee14c4e8
Louis Orleans <louis@orleans.io>
2026-09-04 01:22:10 -0700

    🐛 fix nh cleaner

--- modules/defaults/home-manager/nh.nix
+++ modules/defaults/home-manager/nh.nix
@@ -6,6 +6,9 @@
     flake = "${config.home.homeDirectory}/git/dudeofawesome/nix-config/";
   };
 
+  launchd.agents.nh-clean.config.EnvironmentVariables.PATH =
+    "/nix/var/nix/profiles/default/bin:/usr/bin:/bin:/usr/sbin:/sbin";
+
   home.sessionVariables = {
     NH_SHOW_ACTIVATION_LOGS = "true";
   };
