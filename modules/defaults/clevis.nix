cdeb7065425af8f1caa99ff977d1871c7eeb7441
Louis Orleans <louis@orleans.io>
2026-09-10 18:33:02 -0700

    ➖ rm tsme-test

--- modules/defaults/clevis.nix
+++ modules/defaults/clevis.nix
@@ -1,14 +1,8 @@
 {
-  config,
-  lib,
   pkgs,
   ...
 }:
 {
-  boot.extraModulePackages = lib.mkIf config.boot.initrd.clevis.enable [
-    config.boot.kernelPackages.tsme-test
-  ];
-
   environment.systemPackages = with pkgs; [
     clevis
   ];
