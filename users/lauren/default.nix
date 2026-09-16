2ef49c48622d677e2f85a054767a026edf3e0b72
Louis Orleans <louis@orleans.io>
2026-09-16 16:19:42 -0700

    🔐 add user password for lauren

--- users/lauren/default.nix
+++ users/lauren/default.nix
@@ -3,5 +3,15 @@
     fullName = "Lauren";
     openssh.authorizedKeys.keys = [ ];
   };
+  os = {
+    linux =
+      { config, ... }:
+      {
+        sops.secrets."users/lauren/hashedPassword" = {
+          sopsFile = ./secrets.yaml;
+          neededForUsers = true;
+        };
+      };
+  };
   home-manager = import ./home-manager;
 }
